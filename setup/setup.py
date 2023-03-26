0d334a8be7b243ae704f8c829b39ccc0b6ffaae2
hectoxor <105129772+hectoxor@users.noreply.github.com>
2023-03-26 21:13:36 +0600

    Delete setup.py

--- setup/setup.py
+++
@@ -1,9 +0,0 @@
-import logging
-import openai
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
-from telegram import Update, ForceReply
-
-openai.api_key = "my key"
-
-TELEGRAM_API_KEY = "my key"
