aff34e33312df930e0c80579e5469af07cd85550
hectoxor <105129772+hectoxor@users.noreply.github.com>
2023-03-26 21:13:42 +0600

    Delete analyze-incident.py

--- setup/analyze-incident.py
+++
@@ -1,8 +0,0 @@
-def analyze_incident(incident_description):
-    manager_prompt = f"Analyze the following incident reported by a resident of the property owners' association: '{incident_description}'. Provide a simplified version and suggest some solutions for the manager."
-    resident_prompt = f"Based on the reported incident: '{incident_description}', provide a list of steps the resident can take to improve the situation."
-
-    manager_response = generate_response(manager_prompt, max_tokens=500)
-    resident_response = generate_response(resident_prompt, max_tokens=500)
-
-    return manager_response, resident_response
